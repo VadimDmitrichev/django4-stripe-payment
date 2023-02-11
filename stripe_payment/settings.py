cbe7ee81ce11c8d369e00495fe28947498e153de
Vadim <vadya.dmitrichev26@gmail.com>
2023-02-12 01:21:56 +0300

    update settings.py

--- stripe_payment/settings.py
+++ stripe_payment/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc+e5o7b)_*oz*=51pl#-t_or0n#m%eic1^4+lby-4szq&6375'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django4-stripe-payment-production.up.railway.app']
 
